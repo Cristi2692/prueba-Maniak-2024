77520a58554832c398f57102388a69144966e972
Cristi2692 <c.guerreroheras@gmail.com>
2024-12-15 09:27:49 +0100

    style: Bloqueamos interacciones con la página principal
    Creamos un overlay para bloquear la interacciones con la página principal mientras el carrito de compra está abierto.

--- components/cart/cart-sheet/cart-sheet.js
+++ components/cart/cart-sheet/cart-sheet.js
@@ -41,15 +41,27 @@ function renderCartItems() {
     `;
 }
 
+function createOverlay() {
+  const overlay = document.createElement('div');
+  overlay.className = 'cart-overlay';
+  return overlay;
+}
+
 export function show() {
   const cartSheet = document.querySelector(".cart-sheet");
+  const overlay = createOverlay();
+  document.body.appendChild(overlay);
   document.body.style.overflow = 'hidden';
   renderCartItems();
   cartSheet.classList.add("open");
+  setTimeout(() => overlay.classList.add('active'), 50);
 }
 
 export function close() {
   const cartSheet = document.querySelector(".cart-sheet");
+  const overlay = document.querySelector('.cart-overlay');
+  overlay.classList.remove('active');
   document.body.style.overflow = 'auto';
   cartSheet.classList.remove("open");
+  setTimeout(() => overlay.remove(), 300); // Esperamos a que termine la transición
 }
